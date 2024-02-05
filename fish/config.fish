13dfbcb6f6182da7208c1d24bd669983bb6bafbb
Eray Altun <ealtun21@ku.edu.tr>
2024-02-06 01:03:20 +0300

    visual changed and fish shell

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    eval "$(starship init fish)"
+    cat ~/.cache/wal/sequences
+    PF_INFO="ascii title os kernel uptime pkgs memory" pfetch
+end
+alias hx=helix
+set fish_greeting
+alias ls='eza --icons'
+alias ll='eza -al --icons'
+alias lt='eza -a --tree --level=1 --icons'
+alias update-grub='sudo grub-mkconfig -o /boot/grub/grub.cfg'
